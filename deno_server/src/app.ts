e18cb3656bb9294f5fe738f3e4fc741376fec8d5
Andrew <SolomonAndrew@tutanota.com>
2023-01-12 01:02:44 +0200

    Update deno configuration file
    add lint rules
    add formatting options
    add compiler options

--- deno_server/src/app.ts
+++ deno_server/src/app.ts
@@ -1,36 +1,35 @@
-import { Application } from "../deps.ts";
+import { Application } from '../deps.ts';
 
-import AuthRouter from "./routes/Auth.Routes.ts";
+import AuthRouter from './routes/Auth.Routes.ts';
 
 export default class App {
-    private APP: Application;
-    private PORT: number;
-    private SESSION_SECRET: string | undefined;
-    private ENVIRONMENT: 'development' | 'production';
+	private APP: Application;
+	private PORT: number;
+	private SESSION_SECRET: string | undefined;
+	private ENVIRONMENT: 'development' | 'production';
+	
+	constructor() {
+		this.APP = new Application();
+		this.PORT = 3000;
+		this.SESSION_SECRET = 'I have the high ground';
+		this.ENVIRONMENT = 'development';
 
-    constructor() {
-        this.APP = new Application();
-        this.PORT = 3000;
-        this.SESSION_SECRET = 'I have the high ground';
-        this.ENVIRONMENT = 'development';
+		this.handleRoutes();
+		this.boot();
+	}
 
-        this.handleRoutes();
-        this.boot();
-    }
+	private handleRoutes(): void {
+		this.APP.use(AuthRouter.routes());
+	}
 
-    private handleRoutes(): void {
-        this.APP.use(AuthRouter.routes());
-    }
-
-    private boot(): void {
-        try {
-            this.APP.listen({port: this.PORT});
-        } catch (bootError) {
-            console.log(`An error occured while booting server...`);
-            console.log(bootError);
-        }
-    }
+	private boot(): void {
+		try {
+			this.APP.listen({ port: this.PORT });
+		} catch (bootError) {
+			console.log(`An error occured while booting server...`);
+			console.log(bootError);
+		}
+	}
 }
-
 
 new App();
--- deno_server/src/routes/Auth.Routes.ts
+++ deno_server/src/routes/Auth.Routes.ts
@@ -1,10 +1,8 @@
-import { Router } from "../../deps.ts";
+import { Router } from '../../deps.ts';
 
 const router = new Router({
-    prefix: '/api/v1/auth',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	prefix: '/api/v1/auth',
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
 });
-
-
 
 export default router;
