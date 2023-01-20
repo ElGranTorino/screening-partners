de24e272c91b4abe7e48caeba447914f70e61057
Andrew <SolomonAndrew@tutanota.com>
2023-01-20 03:58:55 +0200

    create breadcrumbs component

---
+++ client/components/atoms/Breadcrumb/index.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+    let props = withDefaults(defineProps<{
+        isActive?: boolean;
+        number?: number;
+    }>(),{
+        isActive: false,
+        number: 1
+    })
+
+
+    const isActive = ref<boolean>(props.isActive);
+    const number = ref<number>(props.number);
+</script>
+
+
+
+<template>
+    <li class="flex items-center justify-end v-breadcrumb">
+        <span 
+            class="circle bg-gray-400 text-white font-semibold rounded-full w-8 h-8 flex text-sm items-center justify-center mr-2"
+            :class="[isActive ? 'bg-blue-700' : 'bg-gray-400']"
+        >{{ number }}</span>
+        <span 
+            class="font-bold tracking-wide text-sm font-montserrat "
+            :class="[!isActive ? 'text-gray-400' : '']"
+        >
+            <slot></slot>
+        </span>
+    </li>
+</template>
+
+<script lang="ts">
+    export default defineComponent({
+        name: 'BreadCrumb'
+    })
+</script>
+
+
+<style>
+.v-breadcrumb:not(:last-of-type)::after {
+    content: '';
+    margin-left: 1rem;
+    display: inline-block;
+    width: 20px;
+    height: 2px;
+    background: silver;
+}
+.v-breadcrumb:not(:first-of-type) {
+    margin-left: 1rem;
+}
+</style>
---
+++ client/components/molecules/Breadcrumbs/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    export interface Crumb {
+        number: number;
+        text: string;
+    }
+    const props = defineProps<{
+        crumbs: Array<Crumb>,
+        activeCrumb: number;
+    }>();
+
+    const crumbs = reactive<Array<Crumb>>(props.crumbs);
+    const activeCrumb = ref<number>(props.activeCrumb);
+</script>
+
+
+<template lang="">
+    <ul class="flex">
+        <AtomsBreadcrumb 
+        v-for="crumb, index in crumbs"
+        :isActive="activeCrumb === crumb.number" 
+        :number="crumb.number"
+        >{{crumb.text}}</AtomsBreadcrumb>
+    </ul>
+</template>
+<script lang="ts">
+import { AtomsBreadcrumb } from '#components';
+export default defineComponent({
+    name: 'BreadCrumbs'
+})
+</script>
