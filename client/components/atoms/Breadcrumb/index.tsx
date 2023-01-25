0b0d753f3bb6d98ed072d7989ccfa962b68820a6
Andrew <SolomonAndrew@tutanota.com>
2023-01-25 03:45:37 +0200

    refactor: rewrite breadcrumbs component using jsx syntax

---
+++ client/components/atoms/Breadcrumb/index.tsx
@@ -0,0 +1,37 @@
+
+
+export default defineComponent({
+    name: 'BreadCrumb',
+    props: {
+        isColored: {
+            type: Boolean, 
+            required: false,
+            default: false
+        }, 
+        num: {
+            type: Number,
+            default: 1,
+            required: true,
+        }
+    },
+    setup(props){
+        const { isColored, num } = reactive(props);
+        
+        return {
+            isColored, num
+        }
+    },
+    render() {
+        const slot = useSlots()?.default?.();
+        return (
+            <li class='flex items-center justify-end v-breadcrumb'>
+                <span
+                    class={`circle bg-gray-400 text-white font-semibold rounded-full w-8 h-8 flex text-sm items-center justify-center mr-2 ${this.isColored ? 'bg-blue-700' : 'bg-gray-400'}`}
+                >{ this.num }</span>
+                <span class={`font-bold tracking-wide text-sm font-montserrat ${!this.isColored ? 'text-gray-400' : ''}`}>
+                    { slot }
+                </span>
+            </li>
+        );
+    }
+});
---
+++ client/components/molecules/Breadcrumbs/index.tsx
@@ -0,0 +1,40 @@
+
+import BreadCrumb from '@/components/atoms/Breadcrumb';
+import { PropType } from 'nuxt/dist/app/compat/capi';
+
+export interface Crumb {
+    text: string;
+    number: number;
+}
+export default defineComponent({
+    props: {
+        crumbs: {
+            type: Object as PropType<Array<Crumb>>,
+            required: true,
+        },
+    },
+    setup(props){
+        const crumbs = reactive<Array<Crumb>>(props.crumbs)
+
+        return {
+            crumbs
+        }
+    },
+    render() {
+        return (
+            <ul class='flex'>
+                {
+                    this.crumbs.map((item, i) => {
+                        return (
+                            <BreadCrumb
+                            key={i}
+                            isColored={true}
+                            num={item.number}
+                            >{item.text}</BreadCrumb>
+                        );
+                    })
+                }
+            </ul>
+        );            
+    },
+})
