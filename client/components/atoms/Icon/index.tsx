c3ac3a61280b49dd32e3e4afeb1d0b97cd461e6e
Andrew <SolomonAndrew@tutanota.com>
2023-01-26 14:22:34 +0200

    create icon component
    create JSON-like util with icons codes

---
+++ client/components/atoms/Icon/index.tsx
@@ -0,0 +1,24 @@
+import { PropType } from "nuxt/dist/app/compat/capi";
+import IconsJSON from "~~/utils/Icons";
+import { TIcons } from "~~/utils/Icons";
+export default defineComponent({
+    props: {
+        name: {
+            type: String as PropType<keyof TIcons>,
+            required: true,
+        }
+    },
+    setup(props) {
+        const name = props.name;
+        const svg = ref(IconsJSON[name]);
+        
+        return {
+            svg,
+        };
+    },
+    render() {
+        return (
+            <span v-html={this.svg}></span>
+        );
+    }
+})
---
+++ client/utils/Icons.ts
@@ -0,0 +1,6 @@
+export interface TIcons {[key: string]: string};
+
+export default {
+    x: `<svg class="fill-current text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22"><path class="heroicon-ui" d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"/></svg>`
+} as TIcons;
+
