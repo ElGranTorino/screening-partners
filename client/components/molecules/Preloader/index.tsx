fa7ee3d5422b128ebf8c9c6510ff6dc4fdeb2948
Andrew <SolomonAndrew@tutanota.com>
2023-01-26 22:38:47 +0200

    create preloader component

---
+++ client/components/molecules/Preloader/index.tsx
@@ -0,0 +1,12 @@
+import Spinner from "~~/components/atoms/Spinner";
+
+export default defineComponent({
+    name: 'Preloader',
+    render() {
+        return (
+            <div class="bg-white absolute w-full h-full z-50">
+                <Spinner class="aboslute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"/>
+            </div>
+        );
+    }
+});
